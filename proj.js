827186887524d262f3457a17c273652942ae512b
Cassey R. <rigodon.cv@obmontessori.edu.ph>
2026-05-12 08:46:13 +0800

    Update proj.js

--- proj.js
+++ proj.js
@@ -1,16 +1,18 @@
 function login() {
     const studentID = "2026-0001";
-    const p = "12345";
-    const s = "9-Sapphire";
+    const password = "12345";
+    const section = "9-Sapphire";
 
     let studentIDInput = document.getElementById("StudentID").value;
-    let passInput = document.getElementById("Password").value;
+    let passwordInput = document.getElementById("Password").value;
     let sectionInput = document.getElementById("Section").value;
 
-    if (studentIDInput === "" || passInput === "" || sectionInput === "") {
+    if (studentIDInput === "" || passwordInput === "" || sectionInput === "") {
         alert("Please fill in all fields.");
-    } else if (studentIDInput === studentID && passInput === p && sectionInput === s) {
+
+    } else if (studentIDInput === studentID && passwordInput === password && sectionInput === section) {
         alert("Login Successful!");
+        
         window.location.href = "dashboard.html";
     } else {
         alert("Please enter valid credentials.");
